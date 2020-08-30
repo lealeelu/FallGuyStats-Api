1403b0651fd0a47694b72a9bd2ad1541d205e003
Kyler Lee <leekydesu@gmail.com>
2020-08-30 13:14:17 -0400

    Adding more round code data

--- FallGuyStats/Models/Round.cs
+++ FallGuyStats/Models/Round.cs
@@ -21,7 +21,31 @@ namespace FallGuyStats.Models
 
         public static SortedDictionary<string, string> RoundTypeMap = new SortedDictionary<string, string>
         {
-            { "gauntlet_01", "Hit Parade" }
+            { "gauntlet_01", "Hit Parade" },
+            { "gauntlet_02", "Dizzy Heights" },
+            { "gauntlet_03", "The Whirlygig" },
+            { "match_fall", "Perfect Match" },
+            { "chompchomp", "Gate Crash" },
+            { "door_dash", "Door Dash" },
+            { "tunnel", "Roll Out" },
+            { "lava", "Slime Climb" },
+            { "tip_toe", "Tip Toe" },
+            { "floor_fall", "Hex-A-Gone" },
+            { "dodge_fall", "Fruit Chute" },
+            { "tail_tag", "Solo Tail Tag" },
+            { "hoops", "Hoopsie Daisy" },
+            { "jump_showdown", "Jump Showdown" },
+            { "jump_club", "Jump Club" },
+            { "conveyor_arena", "Team Tail Tag" },
+            { "block_party", "Block Party" },
+            { "fall_mountain_hub_complete", "Fall Mountain" },
+            { "royal_rumble", "Royal Fumble" },
+            { "fall_ball_60_players", "Fall Ball" },
+            { "jinxed", "Jinxed" },
+            { "unknown5", "Egg Scramble" },
+            { "ballhogs", "Hoarders" },
+            { "rocknroll", "Rock'n'Roll" },
+            { "see_saw", "See Saw" }
         };
     }
 }
