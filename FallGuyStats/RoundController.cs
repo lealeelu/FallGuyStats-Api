d9d97667a48d5954a8f95b06554c5f1f00f2426f
Leah Lee <leah.lee.42@gmail.com>
2020-09-01 00:41:25 -0400

    Add round controller remove .vs files

---
+++ FallGuyStats/RoundController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FallGuyStats.Data;
+using FallGuyStats.Models;
+
+namespace FallGuyStats
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoundController : ControllerBase
+    {
+        private readonly EpisodeContext _context;
+
+        public RoundController(EpisodeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Round
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RoundModel>>> GetRounds()
+        {
+            return await _context.Rounds.ToListAsync();
+        }
+
+        // GET: api/Round/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RoundModel>> GetRoundModel(int id)
+        {
+            var roundModel = await _context.Rounds.FindAsync(id);
+
+            if (roundModel == null)
+            {
+                return NotFound();
+            }
+
+            return roundModel;
+        }
+
+        // PUT: api/Round/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRoundModel(int id, RoundModel roundModel)
+        {
+            if (id != roundModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(roundModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoundModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Round
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<RoundModel>> PostRoundModel(RoundModel roundModel)
+        {
+            _context.Rounds.Add(roundModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRoundModel", new { id = roundModel.Id }, roundModel);
+        }
+
+        // DELETE: api/Round/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RoundModel>> DeleteRoundModel(int id)
+        {
+            var roundModel = await _context.Rounds.FindAsync(id);
+            if (roundModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rounds.Remove(roundModel);
+            await _context.SaveChangesAsync();
+
+            return roundModel;
+        }
+
+        private bool RoundModelExists(int id)
+        {
+            return _context.Rounds.Any(e => e.Id == id);
+        }
+    }
+}
--- FallGuyStats/Services/StatService.cs
+++ FallGuyStats/Services/StatService.cs
@@ -30,46 +30,45 @@ namespace FallGuyStats.Services
             foreach (var newEpisode in newEpisodes)
             {
                 //check timestamps against db to determine if it is actually a new episode
-                if (!_episodeContext.Episodes.Any(episode => episode.Timestamp == newEpisode.Timestamp))
+                if (_episodeContext.Episodes.Any(episode => episode.Timestamp == newEpisode.Timestamp)) return;
+                
+                //convert entity to model
+                var episodeModel = new EpisodeModel
                 {
-                    //convert entity to model
-                    var episodeModel = new EpisodeModel
-                    {
-                        Crowns = newEpisode.Crowns,
-                        Fame = newEpisode.Fame,
-                        Kudos = newEpisode.Kudos,
-                        Timestamp = newEpisode.Timestamp,
-                        Created = DateTime.UtcNow
-                    };
+                    Crowns = newEpisode.Crowns,
+                    Fame = newEpisode.Fame,
+                    Kudos = newEpisode.Kudos,
+                    Timestamp = newEpisode.Timestamp,
+                    Created = DateTime.UtcNow
+                };
 
-                    //add episode model to db
-                    var newEpisodeEntity = _episodeContext.Episodes.Add(episodeModel);
-                    _episodeContext.SaveChanges();
-                    //after the episode has been commited to the db, get the new episode id
-                    var id = _episodeContext.Episodes.SingleOrDefault(e => e.Timestamp == newEpisode.Timestamp).Id;
+                //add episode model to db
+                var newEpisodeEntity = _episodeContext.Episodes.Add(episodeModel);
+                _episodeContext.SaveChanges();
+                //after the episode has been commited to the db, get the new episode id
+                var id = _episodeContext.Episodes.SingleOrDefault(e => e.Timestamp == newEpisode.Timestamp).Id;
                     
-                    //add round models to db
-                    foreach (var round in newEpisode.RoundEntities)
+                //add round models to db
+                foreach (var round in newEpisode.RoundEntities)
+                {
+                    var roundModel = new RoundModel()
                     {
-                        var roundModel = new RoundModel()
-                        {
-                            Kudos = round.Kudos,
-                            Fame = round.Fame,
-                            RoundType = round.RoundType,
-                            Badge = round.Badge,
-                            BonusFame = round.BonusFame,
-                            BonusKudos = round.BonusKudos,
-                            BonusTier = round.BonusTier,
-                            EpisodeId = id,
-                            Position = round.Position,
-                            Qualified = round.Qualified
-                        };
-                        _episodeContext.Rounds.Add(roundModel);
-                    }
-                    _episodeContext.SaveChanges();
+                        Kudos = round.Kudos,
+                        Fame = round.Fame,
+                        RoundType = round.RoundType,
+                        Badge = round.Badge,
+                        BonusFame = round.BonusFame,
+                        BonusKudos = round.BonusKudos,
+                        BonusTier = round.BonusTier,
+                        EpisodeId = id,
+                        Position = round.Position,
+                        Qualified = round.Qualified
+                    };
+                    _episodeContext.Rounds.Add(roundModel);
                 }
+                _episodeContext.SaveChanges();
                 _logger.LogInformation($"Saved {newEpisode.Timestamp} to db");
-            }            
+            }
         }
     }
 }
